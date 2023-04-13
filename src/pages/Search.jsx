a08e4fa9c78d02261108672ad04ca9e7b0456c0d
Matheus Yelgde <yelgde.8@gmail.com>
2023-04-13 19:24:06 -0300

    Requisito 5 completo

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,43 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    isSaveButtonDisabled: true,
+  };
+
+  validation = ({ target: { value } }) => {
+    const length = 2;
+    if (value.length >= length) {
+      this.setState({
+        isSaveButtonDisabled: false,
+      });
+    } else {
+      this.setState({
+        isSaveButtonDisabled: true,
+      });
+    }
+  };
+
   render() {
+    const { isSaveButtonDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <label htmlFor="">
+          <input
+            onChange={ this.validation }
+            data-testid="search-artist-input"
+            type="text"
+            placeholder="Nome Do Artista"
+          />
+        </label>
+        <button
+          disabled={ isSaveButtonDisabled }
+          type="button"
+          data-testid="search-artist-button"
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
