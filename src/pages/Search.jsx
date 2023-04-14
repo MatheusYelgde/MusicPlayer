1656de70bdcd7b173681da740139d72f028b8cd0
Matheus Yelgde <yelgde.8@gmail.com>
2023-04-14 17:32:53 -0300

    Requisitos sem erro de lint

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,42 +1,25 @@
-import { toHaveDescription } from '@testing-library/jest-dom/dist/matchers';
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import Header from '../components/Header';
 import searchAlbumsAPI from '../services/searchAlbumsAPI';
 import Loading from './Loading';
-import AlbunsCard from './AlbunsCard';
 
 class Search extends Component {
   state = {
     validationValue: '',
-    isSaveButtonDisabled: true,
     isLoading: false,
     albums: [],
     artist: '',
   };
 
-  // validation = ({ target: { value } }) => {
-  //   const length = 2;
-  //   if (value.length >= length) {
-  //     this.setState({
-  //       validationValue: value,
-  //       isSaveButtonDisabled: false,
-  //     });
-  //   } else {
-  //     this.setState({
-  //       isSaveButtonDisabled: true,
-  //     });
-  //   }
-  // };
-
-  handleChange = ( { target: { value }}) => {
+  handleChange = ({ target: { value } }) => {
     this.setState({
       validationValue: value,
     });
   };
 
   bringTheArtist = async () => {
-    const { isLoading, validationValue } = this.state;
+    const { validationValue } = this.state;
     this.setState({
       isLoading: true,
       artist: validationValue,
@@ -51,7 +34,11 @@ class Search extends Component {
   };
 
   render() {
-    const { isSaveButtonDisabled, albums, validationValue, isLoading, artist } = this.state;
+    const {
+      albums,
+      validationValue,
+      isLoading,
+      artist } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
@@ -74,19 +61,17 @@ class Search extends Component {
             Pesquisar
           </button>
         )}
-        {albums.length > 0
-          && <h1>Resultado de álbuns de: { artist }</h1>
-        }
+        {albums.length > 0 ? (
+          <h1>
+            Resultado de álbuns de:
+            {' '}
+            {artist}
+          </h1>
+        ) : null}
         {(albums.length === 0 && artist) && <p>Nenhum álbum foi encontrado</p>}
         {albums.map((album) => (
           <div key={ album.id }>
-            {/* <p>{album.artistId}</p> */}
             <p>{album.artistName}</p>
-            {/* <p>{album.collectionId}</p> */}
-            {/* <p>{album.collectionName}</p> */}
-            {/* <p>{album.collectionPrice}</p> */}
-            {/* <p>{album.releaseDate}</p> */}
-            {/* <p>{album.trackCount}</p> */}
             <img src={ album.artworkUrl100 } alt="" />
             <Link
               to={ `/album/${album.collectionId}` }
